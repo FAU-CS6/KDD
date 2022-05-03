9a7632ca1607dab6e0943e657d6c956b5c563f3d
Melanie B. Sigl <melsigl@users.noreply.github.com>
2022-05-03 07:49:05 +0200

    Reviewed Exercise "Data Analysis & Data Preprocessing" (#34)
    * exercise-data: Intialize the main structure - downloading the db is currently bugged
    
    * exercise-data: Fixed the download procedure of the database
    
    * exercise-data: Started the exercise sheet and implemented a github action to compile a student and a solution version out of the Jupyter notebook
    
    * exercise-data: Fix the wrong keywords within the new CI
    
    * exercise-data: Fix the wrong keywords within the new CI (2)
    
    * exercise-data: Fix the wrong make target in the new CI
    
    * exercise-data: Added a small demo notebook to show the different settings to work with the CI
    
    * exercise-data: Finalized the tasks regarding chapter 2.1.1
    
    * exercise-data: Finalized the tasks regarding chapter 2.1.2
    
    * exercise-data: Edited the existing questions to match the Jour Fixe results and introduced some open text questions to section 2.1.1 and 2.1.2
    
    * exercise-data: Implemented chapter 2.1.3 Data Visualization
    
    * exercise-data: Implemented chapter 2.1.4 Data similarity
    
    * exercise-data: First content for chapter 2.2
    
    * exercise-data: Finished 2.2.1 and 2.2.2
    
    * exercise-data: Finished 2.2
    
    * exercise-data: Implemented the first part of chapter 2.3
    
    * exercise-data: Implemented section 2.3.2 Discretization
    
    * exercise-data: Added smoothing to section 2.3.2
    
    * exercise-data: Finalized the first version by adding tasks regarding the PCA
    
    * exercise-data: Splitted the notebook into three smaller ones to make it easier to handle
    
    * exercise-data: reviewed exercise with following modifications:
    - improved wording whenever possible
    - capitalized headings
    - marked names of variables, classes, functions, and columns as inline code for
    readability
    - build database path depending on operating system used instead of hard coded
    separators
    - extended and improved some answers
    - reduced usage of HTML tags as much as possible
    - simplified some Python statements
    - renamed lecture title: Processing -> Data Processing
    
    * exercise-data: removed git submodule "databases" as it is not referenced or used
    explicitly anywhere
    
    * exercise-data: Implement the feedback
    
    * exercise-data: Fixed the missing pre-commit hooks after implementing the feedback
    
    * exercise-ci: Modified the GitHub Actions to now build archives containing all files required for the exercises instead of building single ipynbs
    
    * exercise-data: use pandas capabilities and list comprehension to simplify functions
    
    * exercise-data: Added 'pythonic' sample solutions where adequate
    
    Co-authored-by: Dominik Probst, M. Sc <dominik.probst@fau.de>

---
+++ exercise/create_exercise_archive.py
@@ -0,0 +1,78 @@
+# Imports
+import os
+import tempfile
+import shutil
+import re
+from argparse import ArgumentParser
+from traitlets.config import Config
+from nbconvert.exporters import NotebookExporter
+from nbconvert.preprocessors import TagRemovePreprocessor
+
+# Helper functions to check command line input
+def valid_dir(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+
+def valid_zip_path(string):
+    if os.path.splitext(string)[1] == ".zip":
+        return string
+    else:
+        raise Exception(str(string) + " is not a valid zip path")
+
+
+def valid_style(string):
+    if string == "original" or string == "student" or string == "solution":
+        return string
+    else:
+        raise Exception(str(string) + " is not a valid style")
+
+
+# Main part
+# Handle the command line input
+parser = ArgumentParser()
+parser.add_argument("input", type=valid_dir)
+parser.add_argument("-o", "--output", dest="output", required=True, type=valid_zip_path)
+parser.add_argument("-s", "--style", dest="style", default="original", type=valid_style)
+args = parser.parse_args()
+
+# Copy the input directory into a temp folder
+temp_folder = tempfile.mkdtemp()
+folder_name = os.path.basename(os.path.normpath(args.input))
+folder_temp_path = os.path.join(temp_folder, folder_name)
+shutil.copytree(args.input, folder_temp_path)
+
+# If the style is not "original", apply the nbconvert tag removal to every ipynb in the folder
+if args.style != "original":
+    # Setup the Config
+    c = Config()
+
+    # Setup the TagRemovePreprocessor
+    # For the student style delete the solution tags and for the solution style delete the student tags
+    if args.style == "student":
+        c.TagRemovePreprocessor.remove_cell_tags = ("solution",)
+    elif args.style == "solution":
+        c.TagRemovePreprocessor.remove_cell_tags = ("student",)
+
+    c.TagRemovePreprocessor.enabled = True
+
+    # Setup the Exporter
+    c.NotebookExporter.preprocessors = ["nbconvert.preprocessors.TagRemovePreprocessor"]
+    exporter = NotebookExporter(config=c)
+    exporter.register_preprocessor(TagRemovePreprocessor(config=c), True)
+
+    # Iterate through all files in the copied folder
+    for file in os.listdir(folder_temp_path):
+        # Check if it is a .ipynb file
+        if re.match(r"^[A-Za-z0-9_-]*.ipynb$", file):
+            # Delete the cells with the tag
+            new_notebook = exporter.from_filename(os.path.join(folder_temp_path, file))
+
+            # Write the new file over the old one
+            with open(os.path.join(folder_temp_path, file), "w") as new_file:
+                new_file.write(new_notebook[0])
+
+# Create a zip out of the copied (and modified) folder
+shutil.make_archive(args.output, "zip", folder_temp_path)
