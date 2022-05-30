d34f66b1ad9d72e9db02c1c20dd67fd4a7eed5ad
Melanie B. Sigl <melanie.sigl@fau.de>
2022-05-31 00:08:18 +0200

    exercise-classification: init exercises for decision tree, naive bayes, and adaboost. comes with an additional excursus to Python private attributes (name mangling)

---
+++ exercise/4-Classification/datasets/buys_computer.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+train_buys_computer = pd.DataFrame(
+    [
+        ["<=30", "high", "no", "fair", "no"],
+        ["<=30", "high", "no", "excellent", "no"],
+        ["31-40", "high", "no", "fair", "yes"],
+        [">40", "medium", "no", "fair", "yes"],
+        [">40", "low", "yes", "fair", "yes"],
+        [">40", "low", "yes", "excellent", "no"],
+        ["31-40", "low", "yes", "excellent", "yes"],
+        ["<=30", "medium", "no", "fair", "no"],
+        ["<=30", "low", "no", "fair", "yes"],
+        [">40", "medium", "yes", "fair", "yes"],
+        ["<=30", "medium", "yes", "excellent", "yes"],
+        ["31-40", "medium", "no", "excellent", "yes"],
+        ["31-40", "high", "yes", "fair", "yes"],
+        [">40", "medium", "no", "excellent", "no"],
+    ],
+    columns=["age", "income", "student", "credit_rating", "buys_computer"],
+)
+
+test_buys_computer = pd.DataFrame(
+    [["<=30", "medium", "yes", "fair", "yes"]],
+    columns=["age", "income", "student", "credit_rating", "buys_computer"],
+)
