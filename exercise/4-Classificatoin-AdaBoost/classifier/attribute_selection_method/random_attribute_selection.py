0981d9728f3377d18f8cb91ee46a6d36fde9d8d1
Melanie B. Sigl <melanie.sigl@fau.de>
2022-05-31 00:08:18 +0200

    exercise-classification: init exercises for decision tree, naive bayes, and adaboost. comes with an additional excursus to Python private attributes (name mangling)

---
+++ exercise/4-Classificatoin-AdaBoost/classifier/attribute_selection_method/random_attribute_selection.py
@@ -0,0 +1,9 @@
+from random import random
+
+import pandas as pd
+
+
+def random_attribute_selection(
+    dataset: pd.DataFrame, target_attribute: str, partition_attribute: str
+) -> float:
+    return random()
